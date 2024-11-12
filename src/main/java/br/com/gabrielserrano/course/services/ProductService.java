268c9ec9fab9cce359b5aeb6cf07ea33aa0b27e8
Gabriel Serrano <gabriel.serrano@outlook.com.br>
2024-11-12 19:04:56 -0300

    Product

---
+++ src/main/java/br/com/gabrielserrano/course/services/ProductService.java
@@ -0,0 +1,25 @@
+package br.com.gabrielserrano.course.services;
+
+import br.com.gabrielserrano.course.entities.Product;
+import br.com.gabrielserrano.course.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> product = repository.findById(id);
+        return product.get();
+    }
+}
