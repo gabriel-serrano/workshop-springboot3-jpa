6e5f0e9453c2e3f649456375c0c07a2580a50040
Gabriel Serrano <gabriel.serrano@outlook.com.br>
2024-11-13 08:03:10 -0300

    User insert

--- src/main/java/br/com/gabrielserrano/course/resources/UserResource.java
+++ src/main/java/br/com/gabrielserrano/course/resources/UserResource.java
@@ -4,11 +4,10 @@ import br.com.gabrielserrano.course.entities.User;
 import br.com.gabrielserrano.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,6 +27,17 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 
 }
